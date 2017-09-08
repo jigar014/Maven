cff050f3da1c834cfe05402c59f7f1aecb1d42bd
unknown <jigarmehta014@gmail.com>
2017-09-07 21:38:47 -0400

    Initial Commit

---
+++ src/main/java/com/mave/App.java
@@ -0,0 +1,20 @@
+package com.mave;
+import java.util.Scanner;
+public class App {
+	
+	
+	
+	
+	Scanner scr = new Scanner (System.in);;
+	int cm = scr.nextInt();
+	
+	
+	
+	public double re (double inch)
+	{
+		inch = cm/2.54;
+		return (inch);
+	}
+
+
+}
