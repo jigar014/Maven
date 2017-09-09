a09e4a754053b1632bdda83c61efd69a5e33caaa
LAPTOP-IUSPVIV2\HP <jigarmehta014@gmail.com>
2017-09-09 15:40:27 -0400

    mave

--- src/main/java/com/mave/App.java
+++ src/main/java/com/mave/App.java
@@ -1,20 +1,32 @@
 package com.mave;
+/*Author : Jigar Mehta
+ * about first maven project to be push on git hub through source tree.
+ * Conversion of cm to inch
+ * 
+ */
+
 import java.util.Scanner;
+
 public class App {
-	
-	
-	
-	
-	Scanner scr = new Scanner (System.in);;
-	int cm = scr.nextInt();
-	
-	
-	
-	public double re (double inch)
-	{
-		inch = cm/2.54;
-		return (inch);
+	double cm;
+	double inch;
+
+	Scanner scr = new Scanner(System.in);
+	double input = scr.nextDouble();
+
+	public void conv() {
+		cm = input;
+		inch = cm / 2.54;
+		System.out.println(inch);
 	}
 
+	public static void main(String[] args) {
 
+		System.out.println("enter cm");
+
+		App app = new App();
+
+		app.conv();
+
+	}
 }
